61c066033abe91699c59bd8d5b803f959b23fac5
Zhang Yan <yeahkyo@163.com>
2014-12-26 20:01:36 +0800

    *hotfix)missing file url

--- lib/beacon_attached.rb
+++ lib/beacon_attached.rb
@@ -18,14 +18,18 @@ module BeaconAttached
   module ClassMethods
     def has_beacon_attachment(name, options = {})
       define_method "#{name}_url".to_sym do |style = options[:default_style]|
-        return "" if hex.blank?
-
-        file_name = self.send("#{name}_file_name".to_sym)
-        res = Qiniu::Auth.authorize_download_url("#{options[:qiniu_host]}/#{hex[0]}/#{hex[1]}/#{hex[2]}/#{hex}/#{qiniu_name(style)}.#{tail_fix(style, file_name)}?#{image_size(style)}")
-
-        if res.blank?
-          res = Qiniu::Auth.authorize_download_url("#{options[:qiniu_host]/options[:missing_file]}")
+        if hex.blank?
+          res = ""
+        else
+          file_name = self.send("#{name}_file_name".to_sym)
+          res = Qiniu::Auth.authorize_download_url("#{options[:qiniu_host]}/#{hex[0]}/#{hex[1]}/#{hex[2]}/#{hex}/#{qiniu_name(style)}.#{tail_fix(style, file_name)}?#{image_size(style)}")
         end
+
+        if res.blank? && options[:missing_file].present?
+          res = Qiniu::Auth.authorize_download_url("#{options[:qiniu_host]}/#{options[:missing_file]}")
+        end
+
+        res
       end
 
       define_method :image_size do |style|
